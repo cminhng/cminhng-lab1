74b6fafb9612e4fbc6d9acf15919d88ef077a2bb
christine nguyen <cminhng@cmng-MacBook-Pro.local>
2024-09-23 12:08:13 -0700

    second commit

--- Rotor.java
+++ Rotor.java
@@ -9,15 +9,18 @@ public class Rotor {
         this.startChar = c;
         
         while(!this.rotate());
+        //what does this do
             
     }
     
     public boolean rotate(){
         //TODO
-               
+        //char last = rotorValues.charAt(rotorValues.length()-1);
+        String last = rotorValues.substring(rotorValues.length()-1);
+        rotorValues = last.concat(rotorValues.substring(0, rotorValues.length()-1));
+        return true;
     }
     
-
     public int indexOf(char c){
         //TODO
     }
